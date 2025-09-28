a6ec09e7fee7bb7fadc63fbc79ac67987c8cb27d
Nicolás Pavez <63881641+npavezibarra@users.noreply.github.com>
2025-09-28 08:40:06 -0300

    Move shipping address toggle below billing email

--- templates/checkout/form-billing.php
+++ templates/checkout/form-billing.php
@@ -37,6 +37,18 @@ defined( 'ABSPATH' ) || exit;
 
 		foreach ( $fields as $key => $field ) {
 			woocommerce_form_field( $key, $field, $checkout->get_value( $key ) );
+
+			if ( 'billing_email' === $key && true === WC()->cart->needs_shipping_address() ) {
+				?>
+
+				<h3 id="ship-to-different-address">
+					<label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
+						<input id="ship-to-different-address-checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" <?php checked( apply_filters( 'woocommerce_ship_to_different_address_checked', 'shipping' === get_option( 'woocommerce_ship_to_destination' ) ? 1 : 0 ), 1 ); ?> type="checkbox" name="ship_to_different_address" value="1" /> <span><?php esc_html_e( 'Ship to a different address?', 'woocommerce' ); ?></span>
+					</label>
+				</h3>
+
+				<?php
+			}
 		}
 		?>
 	</div>
