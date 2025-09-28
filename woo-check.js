747f83ac5b34ca6c58ba7782ef6603e376b31972
Nicolás Pavez <63881641+npavezibarra@users.noreply.github.com>
2025-09-28 10:33:00 -0300

    Improve shipping modal initialization

--- woo-check.js
+++ woo-check.js
@@ -1,31 +1,55 @@
-jQuery(document).ready(function($) {
+jQuery(function($) {
+    var SHIPPING_MODAL_NAMESPACE = '.shippingModal';
+
     // Change the label text for the City field in both billing and shipping sections
     $('label[for="billing_city"]').text('Región *');
     $('label[for="shipping_city"]').text('Región *');
 
-    var $shippingCheckbox = $('#ship-to-different-address-checkbox');
-    var $shippingOverlay = $('#shipping-modal-overlay');
-    var $shippingClose = $('#shipping-modal-close');
+    function setupShippingModal() {
+        var $checkoutForm = $('form.checkout.woocommerce-checkout');
+        var $shippingCheckbox = $('#ship-to-different-address-checkbox');
+        var $shippingOverlay = $('#shipping-modal-overlay');
+        var $shippingClose = $('#shipping-modal-close');
 
-    if ($shippingCheckbox.length && $shippingOverlay.length) {
+        if (!$shippingCheckbox.length || !$shippingOverlay.length || !$checkoutForm.length) {
+            return;
+        }
+
+        // Ensure the modal lives at the root of the checkout form for consistent positioning.
+        if ($shippingOverlay.parent()[0] !== $checkoutForm[0]) {
+            $shippingOverlay.appendTo($checkoutForm);
+        }
+
+        var $body = $('body');
+        var $focusable = $shippingOverlay.find('a[href], button:not([disabled]), input:not([disabled]):not([type="hidden"]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"])');
+        var $firstFocusable = $focusable.first();
+        var $lastFocusable = $focusable.last();
+
         var openModal = function() {
             $shippingOverlay.addClass('is-visible').attr('aria-hidden', 'false');
-            $('body').addClass('shipping-modal-open');
+            $body.addClass('shipping-modal-open');
+
+            window.requestAnimationFrame(function() {
+                if ($firstFocusable.length) {
+                    $firstFocusable.trigger('focus');
+                }
+            });
         };
 
         var closeModal = function() {
             $shippingOverlay.removeClass('is-visible').attr('aria-hidden', 'true');
-            $('body').removeClass('shipping-modal-open');
+            $body.removeClass('shipping-modal-open');
         };
 
         var closeModalAndReset = function() {
             closeModal();
+
             if ($shippingCheckbox.is(':checked')) {
                 $shippingCheckbox.prop('checked', false).trigger('change');
             }
         };
 
-        $shippingCheckbox.on('change.shippingModal', function() {
+        $shippingCheckbox.off(SHIPPING_MODAL_NAMESPACE).on('change' + SHIPPING_MODAL_NAMESPACE, function() {
             if ($(this).is(':checked')) {
                 openModal();
             } else {
@@ -33,27 +57,48 @@ jQuery(document).ready(function($) {
             }
         });
 
-        $shippingClose.on('click.shippingModal', function(event) {
+        $shippingClose.off(SHIPPING_MODAL_NAMESPACE).on('click' + SHIPPING_MODAL_NAMESPACE, function(event) {
             event.preventDefault();
             closeModalAndReset();
         });
 
-        $shippingOverlay.on('click.shippingModal', function(event) {
+        $shippingOverlay.off('click' + SHIPPING_MODAL_NAMESPACE).on('click' + SHIPPING_MODAL_NAMESPACE, function(event) {
             if ($(event.target).is($shippingOverlay)) {
                 closeModalAndReset();
             }
         });
 
-        $(document).on('keydown.shippingModal', function(event) {
-            if (event.key === 'Escape' && $shippingOverlay.hasClass('is-visible')) {
+        $(document).off('keydown' + SHIPPING_MODAL_NAMESPACE).on('keydown' + SHIPPING_MODAL_NAMESPACE, function(event) {
+            if (!$shippingOverlay.hasClass('is-visible')) {
+                return;
+            }
+
+            if (event.key === 'Escape') {
+                event.preventDefault();
                 closeModalAndReset();
+                return;
+            }
+
+            if (event.key === 'Tab' && $focusable.length) {
+                if (event.shiftKey) {
+                    if ($(document.activeElement).is($firstFocusable)) {
+                        event.preventDefault();
+                        $lastFocusable.trigger('focus');
+                    }
+                } else if ($(document.activeElement).is($lastFocusable)) {
+                    event.preventDefault();
+                    $firstFocusable.trigger('focus');
+                }
             }
         });
 
         if ($shippingCheckbox.is(':checked')) {
-            $shippingCheckbox.trigger('change');
+            openModal();
         } else {
             closeModal();
         }
     }
+
+    setupShippingModal();
+    $(document.body).on('updated_checkout', setupShippingModal);
 });
