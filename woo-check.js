063ff573111261cd317ca060264e63731dda2a38
Nicolás Pavez <63881641+npavezibarra@users.noreply.github.com>
2025-10-01 10:06:45 -0300

    Fix checkout modal initialisation and hijack default toggles

--- woo-check.js
+++ woo-check.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($) {
+jQuery(function($) {
     // Change the label text for the City field in both billing and shipping sections
     $('label[for="billing_city"]').text('Región *');
     $('label[for="shipping_city"]').text('Región *');
@@ -7,6 +7,7 @@ jQuery(document).ready(function($) {
     $('body.woocommerce-checkout h1.alignwide.wp-block-post-title').attr('id', 'checkout-page-title');
 
     const focusableSelector = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), [tabindex]:not([tabindex="-1"])';
+    const $body = $(document.body);
 
     const modalConfigs = [
         {
@@ -23,126 +24,156 @@ jQuery(document).ready(function($) {
         }
     ];
 
-    modalConfigs.forEach(function(config) {
-        const $form = $(config.formSelector);
+    const initialiseModal = function(config, $form) {
+        let modalData = $form.data('wooCheckModalData');
 
-        if (!$form.length) {
-            return;
-        }
+        if (!modalData) {
+            const $modal = $('<div />', {
+                class: 'checkout-modal',
+                id: config.modalId,
+                'aria-hidden': 'true'
+            });
 
-        const $modal = $('<div />', {
-            class: 'checkout-modal',
-            id: config.modalId,
-            'aria-hidden': 'true'
-        });
+            const $panel = $('<div />', {
+                class: 'checkout-modal__panel',
+                role: 'dialog',
+                'aria-modal': 'true',
+                'aria-label': config.ariaLabel
+            });
 
-        const $panel = $('<div />', {
-            class: 'checkout-modal__panel',
-            role: 'dialog',
-            'aria-modal': 'true',
-            'aria-label': config.ariaLabel
-        });
+            const $close = $('<button />', {
+                type: 'button',
+                class: 'checkout-modal__close',
+                'aria-label': 'Cerrar',
+                html: '&times;'
+            });
 
-        const $close = $('<button />', {
-            type: 'button',
-            class: 'checkout-modal__close',
-            'aria-label': 'Cerrar',
-            html: '&times;'
-        });
+            $form.removeAttr('style');
+            $panel.append($close, $form);
+            $modal.append($panel);
+            $('body').append($modal);
 
-        $form.show().appendTo($panel);
-        $panel.prepend($close);
-        $modal.append($panel);
-        $('body').append($modal);
+            let $activeTrigger = null;
 
-        let $activeTrigger = null;
-
-        const closeModal = function(restoreFocus = true) {
-            if (!$modal.hasClass('is-visible')) {
-                return;
-            }
-
-            $modal.removeClass('is-visible').attr('aria-hidden', 'true');
-
-            if ($activeTrigger && $activeTrigger.length) {
-                $activeTrigger.attr('aria-expanded', 'false');
-
-                if (restoreFocus) {
-                    $activeTrigger.trigger('focus');
+            const closeModal = function(restoreFocus = true) {
+                if (!$modal.hasClass('is-visible')) {
+                    return;
                 }
-            }
 
-            $activeTrigger = null;
+                $modal.removeClass('is-visible').attr('aria-hidden', 'true');
 
-            if (!$('.checkout-modal.is-visible').not($modal).length) {
-                $('body').removeClass('checkout-modal-open');
-            }
-        };
+                if ($activeTrigger && $activeTrigger.length) {
+                    $activeTrigger.attr('aria-expanded', 'false');
 
-        $modal.data('wooCheckClose', closeModal);
+                    if (restoreFocus) {
+                        $activeTrigger.trigger('focus');
+                    }
+                }
 
-        const openModal = function($trigger) {
-            $('.checkout-modal.is-visible').not($modal).each(function() {
-                const $otherModal = $(this);
-                const otherClose = $otherModal.data('wooCheckClose');
+                $activeTrigger = null;
 
-                if (typeof otherClose === 'function') {
-                    otherClose(false);
+                if (!$('.checkout-modal.is-visible').not($modal).length) {
+                    $body.removeClass('checkout-modal-open');
+                }
+            };
+
+            $modal.data('wooCheckClose', closeModal);
+
+            const openModal = function($trigger) {
+                $('.checkout-modal.is-visible').not($modal).each(function() {
+                    const $otherModal = $(this);
+                    const otherClose = $otherModal.data('wooCheckClose');
+
+                    if (typeof otherClose === 'function') {
+                        otherClose(false);
+                    } else {
+                        $otherModal.removeClass('is-visible').attr('aria-hidden', 'true');
+                    }
+                });
+
+                $body.addClass('checkout-modal-open');
+                $modal.addClass('is-visible').attr('aria-hidden', 'false');
+
+                if ($trigger && $trigger.length) {
+                    $activeTrigger = $trigger;
+                    $activeTrigger.attr('aria-expanded', 'true');
                 } else {
-                    $otherModal.removeClass('is-visible').attr('aria-hidden', 'true');
+                    $activeTrigger = null;
+                }
+
+                setTimeout(function() {
+                    const $focusable = $panel.find(focusableSelector).filter(':visible');
+                    const $firstField = $focusable.not('.checkout-modal__close').first();
+
+                    if ($firstField.length) {
+                        $firstField.trigger('focus');
+                    } else {
+                        $close.trigger('focus');
+                    }
+                }, 50);
+            };
+
+            $close.on('click', function() {
+                closeModal();
+            });
+
+            $modal.on('click', function(event) {
+                if (event.target === $modal[0]) {
+                    closeModal();
                 }
             });
 
-            $('body').addClass('checkout-modal-open');
-            $modal.addClass('is-visible').attr('aria-hidden', 'false');
-
-            if ($trigger && $trigger.length) {
-                $activeTrigger = $trigger;
-                $activeTrigger.attr('aria-expanded', 'true');
-            } else {
-                $activeTrigger = null;
-            }
-
-            setTimeout(function() {
-                const $focusable = $panel.find(focusableSelector).filter(':visible');
-                const $firstField = $focusable.not('.checkout-modal__close').first();
-
-                if ($firstField.length) {
-                    $firstField.trigger('focus');
-                } else {
-                    $close.trigger('focus');
+            $(document).on('keydown.' + config.modalId, function(event) {
+                if (event.key === 'Escape' && $modal.hasClass('is-visible')) {
+                    closeModal();
                 }
-            }, 50);
-        };
+            });
 
-        $(document).on('click', config.triggerSelector, function(event) {
+            modalData = {
+                modal: $modal,
+                openModal: openModal,
+                closeModal: closeModal
+            };
+
+            $form.data('wooCheckModalData', modalData);
+        }
+
+        const namespace = '.woo-check-modal-' + config.modalId;
+
+        $body.off('click', config.triggerSelector);
+        $body.off('click' + namespace, config.triggerSelector);
+
+        $body.on('click' + namespace, config.triggerSelector, function(event) {
             event.preventDefault();
-            event.stopImmediatePropagation();
-            openModal($(this));
-        });
-
-        $close.on('click', function() {
-            closeModal();
-        });
-
-        $modal.on('click', function(event) {
-            if ($(event.target).is($modal)) {
-                closeModal();
-            }
-        });
-
-        $(document).on('keydown.' + config.modalId, function(event) {
-            if (event.key === 'Escape' && $modal.hasClass('is-visible')) {
-                closeModal();
-            }
+            event.stopPropagation();
+            modalData.openModal($(this));
         });
 
         $(config.triggerSelector).each(function() {
             const $trigger = $(this);
 
-            if (typeof $trigger.attr('aria-expanded') === 'undefined') {
-                $trigger.attr('aria-expanded', 'false');
-            }
+            $trigger.attr({
+                'aria-controls': config.modalId,
+                'aria-expanded': 'false'
+            });
         });
-    });
+    };
+
+    const initialiseModals = function() {
+        modalConfigs.forEach(function(config) {
+            const $forms = $(config.formSelector);
+
+            if (!$forms.length) {
+                return;
+            }
+
+            $forms.each(function() {
+                initialiseModal(config, $(this));
+            });
+        });
+    };
+
+    initialiseModals();
+    $(window).on('load', initialiseModals);
+    $(document.body).on('updated_checkout', initialiseModals);
 });
