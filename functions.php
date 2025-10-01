d34a352a46bf87f05cd385c282e18cd046bf8168
Nicolás Pavez <63881641+npavezibarra@users.noreply.github.com>
2025-09-30 22:31:12 -0300

    Adjust checkout notices layout

--- functions.php
+++ functions.php
@@ -4,13 +4,9 @@
  * Wrap WooCommerce checkout login and coupon notices inside a flex container.
  */
 add_action( 'woocommerce_before_checkout_form', function() {
-    ?>
-    <div class="checkout-notices-row">
-    <?php
+    echo '<div class="checkout-notices-row">';
 }, 5 );
 
 add_action( 'woocommerce_before_checkout_form', function() {
-    ?>
-    </div>
-    <?php
+    echo '</div>';
 }, 15 );
