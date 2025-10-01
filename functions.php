e4e0eaf5689eed4a99d657174dbf157ad64a0ace
Nicolás Pavez <63881641+npavezibarra@users.noreply.github.com>
2025-09-30 22:35:48 -0300

    Add spacer divider between checkout notices

--- functions.php
+++ functions.php
@@ -7,6 +7,15 @@ add_action( 'woocommerce_before_checkout_form', function() {
     echo '<div class="checkout-notices-row">';
 }, 5 );
 
+if ( function_exists( 'woocommerce_checkout_coupon_form' ) ) {
+    remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
+    add_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 15 );
+}
+
+add_action( 'woocommerce_before_checkout_form', function() {
+    echo '<div id="checkout-notices-divider" aria-hidden="true"></div>';
+}, 13 );
+
 add_action( 'woocommerce_before_checkout_form', function() {
     echo '</div>';
-}, 15 );
+}, 20 );
